01351cb9151d40e5e8d9c3bddba582b197e7513a
Iskandar Sharipov <koklok444@gmail.com>
2024-08-09 09:12:25 +0500

    MINOR: New function: Added ADS class

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 require_once 'bootstrap.php';
 
 $user = new App\User();
+$ads = new App\Ads();
 
-$user->deleteUser(id: 1);
+
+$newAds = $ads->addAds("4 Xonali uy", "nimadurda",2, 1, 1, "Xorazim", 12000, 1);
+
+
 
--- src/Ads.php
+++ src/Ads.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use DateTime;
+use PDO;
+
 class Ads
 {
     private $pdo;
@@ -9,6 +12,48 @@ class Ads
     public function __construct()
     {
         $this->pdo = DB::connect();
+    }
+
+    public function addAds(string   $title,
+                           string   $description,
+                           int      $user_id,
+                           int      $status_id,
+                           int      $branch_id,
+                           string   $address,
+                           float    $price,
+                           int      $rooms): array|bool
+    {
+        $query = "insert into ads (title,description,user_id,status_id,branch_id,address,price,rooms,created_at) 
+                  values (:title, :description, :user_id, :status_id, :branch_id, :address, :price, :rooms, NOW())";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':status_id', $status_id);
+        $stmt->bindParam(':branch_id', $branch_id);
+        $stmt->bindParam(':address', $address);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':rooms', $rooms);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function addBranch(string $name, string $address): void
+    {
+        $query = "insert into branch (name, address) values (:name, :address)";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':address', $address);
+        $stmt->execute();
+    }
+
+    public function addStatus(string $name): void
+    {
+        $query = "insert into status (name) values (:name)";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
     }
 
 }
